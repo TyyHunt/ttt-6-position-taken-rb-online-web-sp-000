cf761a37e426b0c9edfaaaa49400bcf637469e32
Tyler Hunt <tyhunt04@gmail.com>
2019-02-09 00:27:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,6 @@
 # code your #position_taken? method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+index = gets.strip - 1
 def position_taken?(board, index)
   if index != "   "
     return false
